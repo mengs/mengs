8d5cb85d371ede0155e179931d53a9b509397aee
lolaeegmail.com <meng.shi@Meng-Ss-MacBook-Pro.local>
2016-11-03 12:29:38 -0700

    add more files

---
+++ geojson.js
@@ -0,0 +1,359 @@
+var dining = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.79005699999999,
+                    33.360831
+                ]
+            },
+            "properties": {
+                "name": "Flancer's - Gilbert, AZ"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.78956800000003,
+                    33.363617
+                ]
+            },
+            "properties": {
+                "name": "Salad and Go"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.79003990000001,
+                    33.3611828
+                ]
+            },
+            "properties": {
+                "name": "Lulus Taco Shop"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.78997500000003,
+                    33.3566667
+                ]
+            },
+            "properties": {
+                "name": "Lo-Lo's Chicken & Waffles"
+            }
+        }
+    ]
+}
+
+var fun={
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.6896749,
+                    33.3905976,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Superstition Springs Center"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.7711258,
+                    33.35904250000001,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Freestone Trailhead"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.78866800000003,
+                    33.3628938,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Village Center Shopping Center"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.80207330000002,
+                    33.384176,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "AMC Mesa Grand 14"
+            }
+        },
+
+    ]
+}
+var essentials={
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.78545610000003,
+                    33.39447329999999,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Target"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.7575908,
+                    33.3337446,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Target"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.78811719999999,
+                    33.3622974,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Big Lots"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.79167039999999,
+                    33.3789068,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Fry's Food And Drug"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.78783090000002,
+                    33.3816516,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Wells Fargo Bank"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.7903513,
+                    33.3647784,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Chase Bank"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.79163340000002,
+                    33.3650029,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Walgreens"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.79051279999999,
+                    33.3627389,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Wells Fargo Bank"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.79247069999997,
+                    33.3638805,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Smart & Final Extra!"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.78762110000002,
+                    33.37246450000001,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Sam's Club"
+            }
+        }
+    ]
+}
+var schools={
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.78272370000002,
+                    33.368913,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Gilbert Junior High School"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.7812753,
+                    33.35898459999999,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Burk Elementary School"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.76687200000003,
+                    33.3488587,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Gilbert High School"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.7829777,
+                    33.3778539,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Sunrise Preschools - Gilbert"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.77148299999999,
+                    33.3507099,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "Gilbert Arts Academy & Preschool"
+            }
+        }
+    ]
+}
+var home = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -111.78419500000001,
+                    33.361004,
+                    0
+                ]
+            },
+            "properties": {
+                "name": "291 E Tremaine Ave"
+            }
+        }
+    ]
+}
